dcc5c4b2eb8c91bff811766a2583c8003cb08c1e
kiichi7580 <kiichi.nakazato0521@gmail.com>
2024-01-26 16:08:39 +0900

    textfieldウィジェット作成

---
+++ lib/widgets/text_field_input.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+class TextFieldInput extends StatelessWidget {
+  const TextFieldInput ({
+    super.key,
+    required this.textEditingController,
+    this.isPass = false,
+    required this.hintText,
+    required this.textInputType,
+  });
+
+  final TextEditingController textEditingController;
+  final bool isPass;
+  final String hintText;
+  final TextInputType textInputType;
+
+  @override
+  Widget build(BuildContext context) {
+    final inputBorder = OutlineInputBorder(
+      borderSide: Divider.createBorderSide(context),
+    );
+
+    return TextField(
+      controller: textEditingController,
+      decoration: InputDecoration(
+        hintText: hintText,
+        border: inputBorder,
+        focusedBorder: inputBorder,
+        enabledBorder: inputBorder,
+        filled: true,
+        contentPadding: const EdgeInsets.all(8),
+      ),
+      keyboardType: textInputType,
+      obscureText: isPass,
+    );
+  }
+}
