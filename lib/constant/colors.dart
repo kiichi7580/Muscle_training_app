8b6c1e6c0dfbcd6b23406dbf21fdd9bc2e2b9708
kiichi7580 <kiichi.nakazato0521@gmail.com>
2024-01-26 16:05:42 +0900

    Colorクラス設定変更

--- lib/constant/colors.dart
+++ lib/constant/colors.dart
@@ -4,4 +4,10 @@ import 'package:flutter/material.dart';
 
 // 白縹（しろはなだ） → R:223, G:242, B:252
 
-Color mainColor = const Color.fromRGBO(255, 255, 255, 1);
+const mainColor = Color.fromRGBO(255, 255, 255, 1);
+const blackColor = Colors.black45;
+const secondaryColor = Color.fromRGBO(155, 218, 242, 1);
+const blueColor = Color.fromRGBO(33, 150, 243, 1);
+const heavyBlueColor = Color.fromRGBO(23, 102, 166, 1);
+// Twitterのリンクの色
+const linkBlue = Color.fromRGBO(27, 149, 224, 1);
