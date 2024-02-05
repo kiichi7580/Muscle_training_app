6e9dde2e9dee33db6b69e454826284b2f88a11a2
kiichi7580 <kiichi.nakazato0521@gmail.com>
2024-02-06 01:32:10 +0900

    メニュー２作成

---
+++ lib/view/drawer/beginner_menu/menu/menu_2.dart
@@ -0,0 +1,160 @@
+import 'package:flutter/cupertino.dart';
+
+class Menu2 extends StatelessWidget {
+  const Menu2({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          '2.クランチ（腹筋）',
+          style: TextStyle(
+            fontSize: 20,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        SizedBox(
+          height: 16,
+        ),
+        Text(
+          ' お腹周りが気になるなら、クランチがおすすめです。',
+          style: TextStyle(
+            fontSize: 18,
+          ),
+        ),
+        SizedBox(
+          height: 24,
+        ),
+        Text(
+          '〈やり方〉',
+          style: TextStyle(
+            fontSize: 20,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        SizedBox(
+          height: 16,
+        ),
+        Text(
+          '(STEP1)',
+          style: TextStyle(
+            fontSize: 18,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        Text(
+          '仰向けで、太ももが床と垂直になるよう足を浮かせる',
+          style: TextStyle(
+            fontSize: 18,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        SizedBox(
+          height: 8,
+        ),
+        Text(
+          '(STEP2)',
+          style: TextStyle(
+            fontSize: 18,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        Text(
+          '両手を頭の後ろで組み、肩は床から浮かせ、視線はおへそを見る',
+          style: TextStyle(
+            fontSize: 18,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        SizedBox(
+          height: 8,
+        ),
+        Text(
+          '(STEP3)',
+          style: TextStyle(
+            fontSize: 18,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        Text(
+          '息を吐きながら、上体を丸め込むイメージで頭を膝に近づける',
+          style: TextStyle(
+            fontSize: 18,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        SizedBox(
+          height: 8,
+        ),
+        Text(
+          '(STEP4)',
+          style: TextStyle(
+            fontSize: 18,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        Text(
+          '息を吸いながら、ゆっくりと元の位置に戻す',
+          style: TextStyle(
+            fontSize: 18,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        SizedBox(
+          height: 24,
+        ),
+        Text(
+          ' 10回を1セットとして3セット挑戦してみてください。',
+          style: TextStyle(
+            fontSize: 18,
+          ),
+        ),
+        SizedBox(
+          height: 24,
+        ),
+        Text(
+          '〈トレーニングのコツ〉',
+          style: TextStyle(
+            fontSize: 20,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        SizedBox(
+          height: 16,
+        ),
+        Text(
+          '・勢いや反動を使わない',
+          style: TextStyle(
+            fontSize: 18,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        SizedBox(
+          height: 8,
+        ),
+        Text(
+          '・肘は固定させる',
+          style: TextStyle(
+            fontSize: 18,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        SizedBox(
+          height: 8,
+        ),
+        Text(
+          '・下げるときはゆっくりと',
+          style: TextStyle(
+            fontSize: 18,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        SizedBox(
+          height: 32,
+        ),
+      ],
+    );
+  }
+}
