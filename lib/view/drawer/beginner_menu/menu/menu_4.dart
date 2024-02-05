1594a81d1bbe92975cb1039a2766b9161ee5f161
kiichi7580 <kiichi.nakazato0521@gmail.com>
2024-02-06 01:32:34 +0900

    メニュー4作成

---
+++ lib/view/drawer/beginner_menu/menu/menu_4.dart
@@ -0,0 +1,177 @@
+import 'package:flutter/cupertino.dart';
+
+class Menu4 extends StatelessWidget {
+  const Menu4({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          '4.ヒップリフト',
+          style: TextStyle(
+            fontSize: 20,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        SizedBox(
+          height: 16,
+        ),
+        Text(
+          ' ヒップリフトに効果があるのは、お尻の大臀筋。そして腰の脊柱起立筋です。体幹を鍛える方法として注目されています。',
+          style: TextStyle(
+            fontSize: 18,
+          ),
+        ),
+        SizedBox(
+          height: 24,
+        ),
+        Text(
+          '〈やり方〉',
+          style: TextStyle(
+            fontSize: 20,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        SizedBox(
+          height: 16,
+        ),
+        Text(
+          '(STEP1)',
+          style: TextStyle(
+            fontSize: 18,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        Text(
+          '仰向けに寝て、膝を曲げる',
+          style: TextStyle(
+            fontSize: 18,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        SizedBox(
+          height: 8,
+        ),
+        Text(
+          '(STEP2)',
+          style: TextStyle(
+            fontSize: 18,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        Text(
+          '手は軽く開いてリラックスする',
+          style: TextStyle(
+            fontSize: 18,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        SizedBox(
+          height: 8,
+        ),
+        Text(
+          '(STEP3)',
+          style: TextStyle(
+            fontSize: 18,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        Text(
+          'お尻に力を入れながらグッと持ち上げる',
+          style: TextStyle(
+            fontSize: 18,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        SizedBox(
+          height: 8,
+        ),
+        Text(
+          '(STEP4)',
+          style: TextStyle(
+            fontSize: 18,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        Text(
+          '肩、お腹、膝が一直線になるまでお尻を上げ、キープする',
+          style: TextStyle(
+            fontSize: 18,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        SizedBox(
+          height: 8,
+        ),
+        Text(
+          '(STEP5)',
+          style: TextStyle(
+            fontSize: 18,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        Text(
+          'ゆっくりとお尻を下げる',
+          style: TextStyle(
+            fontSize: 18,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        SizedBox(
+          height: 24,
+        ),
+        Text(
+          ' お尻の上げ下げを10〜15回、3セットおこないましょう。',
+          style: TextStyle(
+            fontSize: 18,
+          ),
+        ),
+        SizedBox(
+          height: 24,
+        ),
+        Text(
+          '〈トレーニングのコツ〉',
+          style: TextStyle(
+            fontSize: 20,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        SizedBox(
+          height: 16,
+        ),
+        Text(
+          '・お尻を上げる時に息をゆっくりと吐き、下ろす時に息を吸い込む',
+          style: TextStyle(
+            fontSize: 18,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        SizedBox(
+          height: 8,
+        ),
+        Text(
+          '・上半身はリラックスする',
+          style: TextStyle(
+            fontSize: 18,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        SizedBox(
+          height: 8,
+        ),
+        Text(
+          '・ゆっくりとした動作でおこなう',
+          style: TextStyle(
+            fontSize: 18,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        SizedBox(
+          height: 32,
+        ),
+      ],
+    );
+  }
+}
