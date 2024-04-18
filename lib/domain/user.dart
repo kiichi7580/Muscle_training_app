a3b49ff6b609848f7f4f44cf340e808ed3f43635
kiichi7580 <kiichi.nakazato0521@gmail.com>
2024-04-19 01:49:42 +0900

    UserクラスにcreateAtプロパティを追加、fromSnap関数でスナップショット型で取得したデータをUser型に変換

--- lib/domain/user.dart
+++ lib/domain/user.dart
@@ -1,13 +1,25 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
 class User {
   final String email;
   final String uid;
-  final DateTime createAt;
+  final Timestamp createAt;
 
   const User({
     required this.email,
     required this.uid,
     required this.createAt,
   });
+
+  static User fromSnap(DocumentSnapshot snap) {
+    var snapshot = (snap.data() as Map<String, dynamic>);
+
+    return User(
+      email: snapshot['email'],
+      uid: snapshot['uid'],
+      createAt: snapshot['createAt'],
+    );
+  }
 
   Map<String, dynamic> toJson() => {
         'email': email,
