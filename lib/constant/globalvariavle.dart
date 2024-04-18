bc886b53b7baa7aa3d1bcd75471b48014eb8ac96
kiichi7580 <kiichi.nakazato0521@gmail.com>
2024-04-19 01:44:00 +0900

    webScreenSizeの値追加、homeScreenItemsでページにCalendarPage、MemoPage、TimerPageにuidを持たせるようにした

---
+++ lib/constant/globalvariavle.dart
@@ -0,0 +1,13 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:muscle_training_app/view/calendar/calendar_page.dart';
+import 'package:muscle_training_app/view/memo/memo_page.dart';
+import 'package:muscle_training_app/view/timer/timer_page.dart';
+
+const webScreenSize = 600;
+
+List<Widget> homeScreenItems = [
+  CalendarPage(uid: FirebaseAuth.instance.currentUser!.uid),
+  MemoPage(uid: FirebaseAuth.instance.currentUser!.uid),
+  TimerPage(uid: FirebaseAuth.instance.currentUser!.uid),
+];
