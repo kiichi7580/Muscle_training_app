6d661b41fda26ea38ac00fbd0a8bb61fd0b55bbd
kiichi7580 <kiichi.nakazato0521@gmail.com>
2024-04-19 01:48:11 +0900

    Timerクラスの書き方を変更、fromSnap関数でスナップショット型で取得したデータをTimer型に変換、toJson関数でTimer型のデータをjson型に変換

--- lib/domain/timer.dart
+++ lib/domain/timer.dart
@@ -1,8 +1,45 @@
-class MyTimer {
-  MyTimer(this.id, this.timerName, this.totalSecond, this.minute, this.second);
-  String id;
-  String timerName;
-  int totalSecond;
-  String minute;
-  String second;
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class Timer {
+  final String id;
+  final String uid;
+  final String timerName;
+  final int totalSeconds;
+  final String minute;
+  final String second;
+  final DateTime createAt;
+
+  const Timer({
+    required this.id,
+    required this.uid,
+    required this.timerName,
+    required this.totalSeconds,
+    required this.minute,
+    required this.second,
+    required this.createAt,
+  });
+
+  static Timer fromSnap(DocumentSnapshot snap) {
+    var snapshot = snap.data() as Map<String, dynamic>;
+
+    return Timer(
+      id: snapshot['id'],
+      uid: snapshot['uid'],
+      timerName: snapshot['timerName'],
+      totalSeconds: snapshot['totalSeconds'],
+      minute: snapshot['minute'],
+      second: snapshot['second'],
+      createAt: snapshot['createAt'],
+    );
+  }
+
+  Map<String, dynamic> toJson() => {
+        'id': id,
+        'uid': uid,
+        'timerName': timerName,
+        'totalSeconds': totalSeconds,
+        'minute': minute,
+        'second': second,
+        'createAt': createAt,
+      };
 }
