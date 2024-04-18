2577c735353d7af3b4f0f7a48c451381c8d2c39d
kiichi7580 <kiichi.nakazato0521@gmail.com>
2024-04-19 01:47:47 +0900

    Memoクラスの書き方を変更、fromSnap関数でスナップショット型で取得したデータをMemo型に変換、toJson関数でMemo型のデータをjson型に変換

--- lib/domain/memo.dart
+++ lib/domain/memo.dart
@@ -1,10 +1,49 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
 class Memo {
-  Memo(this.id, this.dateId, this.time, this.event, this.weight, this.set, this.rep);
-  String id;
-  dynamic time;
-  int dateId;
-  String event;
-  String weight;
-  String set;
-  String rep;
+  final String id;
+  final String event;
+  final String weight;
+  final String set;
+  final String rep;
+  final int dateId;
+  final DateTime time;
+  final String uid;
+
+  const Memo({
+    required this.id,
+    required this.event,
+    required this.weight,
+    required this.set,
+    required this.rep,
+    required this.dateId,
+    required this.time,
+    required this.uid,
+  });
+
+  static Memo fromSnap(DocumentSnapshot snap) {
+    var snapshot = snap.data() as Map<String, dynamic>;
+
+    return Memo(
+      id: snapshot['id'],
+      event: snapshot['event'],
+      weight: snapshot['weight'],
+      set: snapshot['set'],
+      rep: snapshot['rep'],
+      dateId: snapshot['dateId'],
+      time: snapshot['time'],
+      uid: snapshot['uid'],
+    );
+  }
+
+  Map<String, dynamic> toJson() => {
+        'id': id,
+        'event': event,
+        'weight': weight,
+        'set': set,
+        'rep': rep,
+        'dateId': dateId,
+        'time': time,
+        'uid': uid,
+      };
 }
