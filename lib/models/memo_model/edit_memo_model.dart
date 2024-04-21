3cb5d666d9d65cf73ced97e8dfe3bb8f5961fdee
kiichi7580 <kiichi.nakazato0521@gmail.com>
2024-04-21 14:06:49 +0900

    メモを編集する際、MemoFireStoreMethodsへデータを渡すようにした

--- lib/models/memo_model/edit_memo_model.dart
+++ lib/models/memo_model/edit_memo_model.dart
@@ -1,25 +1,24 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
-import 'package:muscle_training_app/domain/memo.dart';
+import 'package:muscle_training_app/resources/memo_firestore_methods.dart';
 
 class EditMemoModel extends ChangeNotifier {
   EditMemoModel(this.memo) {
-    eventController.text = memo.event;
-    weightController.text = memo.weight;
-    setController.text = memo.set;
-    repController.text = memo.rep;
+    eventController.text = memo['event'];
+    weightController.text = memo['weight'];
+    setController.text = memo['set'];
+    repController.text = memo['rep'];
   }
-  final Memo memo;
+  final dynamic memo;
 
   final eventController = TextEditingController();
   final weightController = TextEditingController();
   final setController = TextEditingController();
   final repController = TextEditingController();
 
-  String? event;
-  String? weight;
-  String? set;
-  String? rep;
+  String event = '';
+  String weight = '';
+  String set = '';
+  String rep = '';
 
   void setEvent(String event) {
     this.event = event;
@@ -41,22 +40,14 @@ class EditMemoModel extends ChangeNotifier {
     notifyListeners();
   }
 
-  bool isUpdated() {
-    return event != null || weight != null || set != null || rep != null;
-  }
-
-  Future<void> update() async {
+  Future<String> update() async {
     this.event = eventController.text;
     this.weight = weightController.text;
     this.set = setController.text;
     this.rep = repController.text;
 
-    // firestoreに追加
-    await FirebaseFirestore.instance.collection('memos').doc(memo.id).update({
-      'event': event,
-      'weight': weight,
-      'set': set,
-      'rep': rep,
-    });
+    String res = await MemoFireStoreMethods()
+        .upDateMemo(event, weight, set, rep, memo.id);
+    return res;
   }
 }
