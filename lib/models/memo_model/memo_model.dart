30ed559a24d6fcc690c459b27f96640b32cd24cb
kiichi7580 <kiichi.nakazato0521@gmail.com>
2024-04-21 14:08:34 +0900

    delete関数の削除、uidデータを持つようにしたので、fetchMemoの内容を修正

--- lib/models/memo_model/memo_model.dart
+++ lib/models/memo_model/memo_model.dart
@@ -1,36 +1,35 @@
 import 'dart:async';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:muscle_training_app/domain/memo.dart';
+
 
 class MemoModel extends ChangeNotifier {
   List<dynamic>? memos;
 
   final Stream<QuerySnapshot> _usersStream = FirebaseFirestore.instance
       .collection('memos')
+      .where('uid', isEqualTo: FirebaseAuth.instance.currentUser!.uid)
       .orderBy('dateId', descending: true)
       .snapshots();
 
   Future<void> fetchMemo() async {
-    _usersStream.listen((snapshot) {
-      final List<dynamic> memos = [];
-      for (var i = 0; i < snapshot.docs.length; i++) {
-        // for (var document in snapshot.docs) {
-        final document = snapshot.docs[i];
-        final dynamic time = document['time'] as dynamic;
-        if (i == 0) {
-          memos.add(time);
-        } else if (!memos.contains(time)) {
-          memos.add(time);
-        } else {}
-      }
+    _usersStream.listen(
+      (snapshot) {
+        final List<dynamic> memos = [];
+        for (var i = 0; i < snapshot.docs.length; i++) {
+          final document = snapshot.docs[i];
+          final dynamic time = document['time'] as dynamic;
+          if (i == 0) {
+            memos.add(time);
+          } else if (!memos.contains(time)) {
+            memos.add(time);
+          } else {}
+        }
 
-      this.memos = memos;
-      notifyListeners();
-    });
-  }
-
-  Future<void> delete(Memo memo) {
-    return FirebaseFirestore.instance.collection('memos').doc(memo.id).delete();
+        this.memos = memos;
+        notifyListeners();
+      },
+    );
   }
 }
