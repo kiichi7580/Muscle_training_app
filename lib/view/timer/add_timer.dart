4aeae0a20f07d42b67f382af438cdfffb28294f1
kiichi7580 <kiichi.nakazato0521@gmail.com>
2024-04-21 14:17:38 +0900

    AddTimerModelの使用をやめ、TimerFireStoreMethodsでメモを追加するようにした

--- lib/view/timer/add_timer.dart
+++ lib/view/timer/add_timer.dart
@@ -1,156 +1,193 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:muscle_training_app/constant/colors.dart';
-import 'package:muscle_training_app/models/timer_model/add_timer_model.dart';
+import 'package:muscle_training_app/domain/user.dart';
+import 'package:muscle_training_app/providers/user_provider.dart';
+import 'package:muscle_training_app/resources/timer_firestore_methods.dart';
+import 'package:muscle_training_app/widgets/show_snackbar.dart';
 import 'package:provider/provider.dart';
 
-class AddTimerPage extends StatelessWidget {
+class AddTimerPage extends StatefulWidget {
   const AddTimerPage({super.key});
 
   @override
+  State<AddTimerPage> createState() => _AddTimerPageState();
+}
+
+class _AddTimerPageState extends State<AddTimerPage> {
+  final TextEditingController _nameController = TextEditingController();
+  final TextEditingController _minuteController = TextEditingController();
+  final TextEditingController _secondController = TextEditingController();
+  bool _isLoading = false;
+
+  Future<void> addTimer(
+    BuildContext context,
+    String uid,
+    String name,
+    String minute,
+    String second,
+  ) async {
+    try {
+      setState(() {
+        _isLoading = true;
+      });
+      String res = await TimerFireStoreMethods().addTimer(
+        uid,
+        name,
+        minute,
+        second,
+      );
+
+      if (res == 'success') {
+        setState(() {
+          _isLoading = false;
+        });
+        showSnackBar('タイマーを追加しました！', context);
+      } else {
+        setState(() {
+          _isLoading = false;
+        });
+        showSnackBar(res, context);
+      }
+    } catch (e) {
+      showSnackBar(e.toString(), context);
+    }
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _nameController.dispose();
+    _minuteController.dispose();
+    _secondController.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return ChangeNotifierProvider<AddTimerModel>(
-      create: (_) => AddTimerModel(),
-      child: Scaffold(
-        appBar: AppBar(
-          title: const Text(
-            'タイマーを追加',
-            style: TextStyle(
-              color: blackColor,
-            ),
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          'タイマーを追加',
+          style: TextStyle(
+            color: blackColor,
           ),
-          backgroundColor: blueColor,
         ),
-        backgroundColor: mainColor,
-        body: Center(
-          child: Consumer<AddTimerModel>(
-            builder: (context, model, child) {
-              return Center(
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Padding(
+        backgroundColor: blueColor,
+      ),
+      backgroundColor: mainColor,
+      body: buildBody(context),
+    );
+  }
+
+  Widget buildBody(BuildContext context) {
+    final User user = Provider.of<UserProvider>(context).getUser;
+
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(8),
+            child: Container(
+              padding: const EdgeInsets.all(16),
+              child: Column(
+                children: [
+                  SizedBox(
+                    height: 90,
+                    width: 294,
+                    child: Padding(
                       padding: const EdgeInsets.all(8),
-                      child: Container(
-                        padding: const EdgeInsets.all(16),
-                        child: Column(
-                          children: [
-                            SizedBox(
-                              height: 90,
-                              width: 294,
-                              child: Padding(
-                                padding: const EdgeInsets.all(8),
-                                child: TextField(
-                                  keyboardType: TextInputType.text,
-                                  decoration: const InputDecoration(
-                                    labelText: '名前',
-                                    border: OutlineInputBorder(),
-                                  ),
-                                  onChanged: (text) {
-                                    model.timerName = text;
-                                  },
-                                ),
-                              ),
-                            ),
-                            const SizedBox(
-                              height: 8,
-                            ),
-                            Row(
-                              mainAxisAlignment: MainAxisAlignment.center,
-                              children: [
-                                SizedBox(
-                                  height: 90,
-                                  width: 130,
-                                  child: Padding(
-                                    padding: const EdgeInsets.all(8),
-                                    child: TextField(
-                                      keyboardType: TextInputType.number,
-                                      decoration: const InputDecoration(
-                                        hintText: '分数を入力',
-                                        border: OutlineInputBorder(),
-                                      ),
-                                      onChanged: (text) {
-                                        model.minute = text;
-                                      },
-                                    ),
-                                  ),
-                                ),
-                                const SizedBox(
-                                  height: 90,
-                                  child: Padding(
-                                    padding: EdgeInsets.all(10),
-                                    child: Text(
-                                      ':',
-                                      style: TextStyle(fontSize: 40),
-                                    ),
-                                  ),
-                                ),
-                                SizedBox(
-                                  height: 90,
-                                  width: 130,
-                                  child: Padding(
-                                    padding: const EdgeInsets.all(8),
-                                    child: TextField(
-                                      keyboardType: TextInputType.number,
-                                      decoration: const InputDecoration(
-                                        hintText: '秒数を入力',
-                                        border: OutlineInputBorder(),
-                                      ),
-                                      onChanged: (text) {
-                                        model.second = text;
-                                      },
-                                    ),
-                                  ),
-                                ),
-                              ],
-                            ),
-                            const SizedBox(
-                              height: 50,
-                              width: double.infinity,
-                            ),
-                            SizedBox(
-                              height: 50,
-                              width: 200,
-                              child: CupertinoButton(
-                                color: blueColor,
-                                onPressed: () async {
-                                  try {
-                                    model.startLoding();
-                                    await model.addTimer();
-                                    Navigator.of(context).pop();
-                                  } catch (e) {
-                                    print(e);
-                                    final snackBar = SnackBar(
-                                      backgroundColor: blackColor,
-                                      content: Text(
-                                        e.toString(),
-                                      ),
-                                    );
-                                    ScaffoldMessenger.of(context)
-                                        .showSnackBar(snackBar);
-                                  } finally {
-                                    model.endLoding();
-                                  }
-                                },
-                                child: const Text(
-                                  '保存する',
-                                  style: TextStyle(
-                                    fontSize: 18,
-                                    fontWeight: FontWeight.w600,
-                                  ),
-                                ),
-                              ),
-                            ),
-                          ],
+                      child: TextField(
+                        controller: _nameController,
+                        keyboardType: TextInputType.text,
+                        decoration: const InputDecoration(
+                          labelText: '名前',
+                          border: OutlineInputBorder(),
                         ),
                       ),
                     ),
-                  ],
-                ),
-              );
-            },
+                  ),
+                  const SizedBox(
+                    height: 8,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      SizedBox(
+                        height: 90,
+                        width: 130,
+                        child: Padding(
+                          padding: const EdgeInsets.all(8),
+                          child: TextField(
+                            controller: _minuteController,
+                            keyboardType: TextInputType.number,
+                            decoration: const InputDecoration(
+                              hintText: '分数を入力',
+                              border: OutlineInputBorder(),
+                            ),
+                          ),
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 90,
+                        child: Padding(
+                          padding: EdgeInsets.all(10),
+                          child: Text(
+                            ':',
+                            style: TextStyle(fontSize: 40),
+                          ),
+                        ),
+                      ),
+                      SizedBox(
+                        height: 90,
+                        width: 130,
+                        child: Padding(
+                          padding: const EdgeInsets.all(8),
+                          child: TextField(
+                            controller: _secondController,
+                            keyboardType: TextInputType.number,
+                            decoration: const InputDecoration(
+                              hintText: '秒数を入力',
+                              border: OutlineInputBorder(),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                  const SizedBox(
+                    height: 50,
+                    width: double.infinity,
+                  ),
+                  SizedBox(
+                    height: 50,
+                    width: 200,
+                    child: CupertinoButton(
+                      color: blueColor,
+                      onPressed: () async {
+                        await addTimer(
+                          context,
+                          user.uid,
+                          _nameController.text,
+                          _minuteController.text,
+                          _secondController.text,
+                        );
+                        Navigator.of(context).pop();
+                      },
+                      child: const Text(
+                        '保存する',
+                        style: TextStyle(
+                          fontSize: 18,
+                          fontWeight: FontWeight.w600,
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
           ),
-        ),
+        ],
       ),
     );
   }
