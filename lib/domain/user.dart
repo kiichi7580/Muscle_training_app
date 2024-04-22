fc31b3f9055a46db0b965bdee1e996424369c686
kiichi7580 <kiichi.nakazato0521@gmail.com>
2024-04-23 00:02:08 +0900

    userクラスにusername、photoUrl、description、followers、followingプロパティを追加

--- lib/domain/user.dart
+++ lib/domain/user.dart
@@ -3,12 +3,22 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 class User {
   final String email;
   final String uid;
+  final String username;
+  final String photoUrl;
+  final String description;
   final DateTime createAt;
+  final List followers;
+  final List following;
 
   const User({
     required this.email,
     required this.uid,
+    required this.username,
+    required this.photoUrl,
+    required this.description,
     required this.createAt,
+    required this.followers,
+    required this.following,
   });
 
   static User fromSnap(DocumentSnapshot snap) {
@@ -19,13 +29,23 @@ class User {
     return User(
       email: snapshot['email'],
       uid: snapshot['uid'],
+      username: snapshot['username'],
+      photoUrl: snapshot['photoUrl'],
+      description: snapshot['description'],
       createAt: createdAt,
+      followers: snapshot['followers'],
+      following: snapshot['following'],
     );
   }
 
   Map<String, dynamic> toJson() => {
         'email': email,
         'uid': uid,
+        'username': username,
+        'photoUrl': photoUrl,
+        'description': description,
         'createAt': createAt,
+        'followers': followers,
+        'following': following,
       };
 }
