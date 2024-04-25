a0246dbfdbb81ed6e5a0adea66b4f017a14423a2
kiichi7580 <kiichi.nakazato0521@gmail.com>
2024-04-26 04:07:48 +0900

    ボーダーに使うborderColorと、トレーニング量を濃さで表すカラーのリストtrainingFrequencyColorsを追加

--- lib/constant/colors.dart
+++ lib/constant/colors.dart
@@ -5,6 +5,7 @@ import 'package:flutter/material.dart';
 // 白縹（しろはなだ） → R:223, G:242, B:252
 
 const mainColor = Color.fromRGBO(255, 255, 255, 1);
+const borderColor = Colors.black12;
 const blackColor = Colors.black54;
 const secondaryColor = Color.fromRGBO(155, 218, 242, 1);
 const blueColor = Color.fromRGBO(33, 150, 243, 1);
@@ -12,3 +13,18 @@ const heavyBlueColor = Color.fromRGBO(23, 102, 166, 1);
 const richBlue = Color.fromRGBO(0, 55, 89, 1);
 // Twitterのリンクの色
 const linkBlue = Color.fromRGBO(27, 149, 224, 1);
+
+// githubの草の色(ライトモード順)
+const trainingFrequencyColor1 = Color.fromRGBO(235, 237, 240, 1);
+const trainingFrequencyColor2 = Color.fromRGBO(155, 233, 168, 1);
+const trainingFrequencyColor3 = Color.fromRGBO(64, 196, 99, 1);
+const trainingFrequencyColor4 = Color.fromRGBO(48, 161, 78, 1);
+const trainingFrequencyColor5 = Color.fromRGBO(33, 110, 57, 1);
+
+final List trainingFrequencyColors = [
+  trainingFrequencyColor1,
+  trainingFrequencyColor2,
+  trainingFrequencyColor3,
+  trainingFrequencyColor4,
+  trainingFrequencyColor5,
+];
