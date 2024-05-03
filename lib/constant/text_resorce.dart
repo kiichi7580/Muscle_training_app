00bedac66a13c9af6ac3e4e621c4947410ea198f
kiichi7580 <kiichi.nakazato0521@gmail.com>
2024-05-04 00:13:02 +0900

    テキストソースを追加

--- lib/constant/text_resorce.dart
+++ lib/constant/text_resorce.dart
@@ -1,1 +1,47 @@
 const defaultPhotoUrlString = 'assets/icons/1024 1.png';
+
+// レスポンス
+const successRes = 'success';
+const successAdd = '追加が完了しました。';
+const successUpDate = '更新が完了しました。';
+const successDelete = '削除が完了しました。';
+
+const failureRes = 'failure';
+const failureAdd = '追加できませんでした。もう一度やり直してください。';
+const failureUpDate = '更新できませんでした。もう一度やり直してください。';
+const failureDelete = '削除できませんでした。もう一度やり直してください。';
+
+// CRUDサイクル
+
+// Create
+const addTx = '追加';
+const createTx = '作成';
+const saveTx = '保存';
+
+// Update
+const upDateTx = '更新';
+
+// Delete
+const deleteTx = '削除';
+
+// バリデーション
+const validationRes = '未入力の項目があります。';
+
+// 新規登録
+const successSignUp = '新規登録に成功しました！';
+const failureSignUp = '問題が発生しました。もう一度、新規登録をしてください。';
+
+// ログイン
+const successLogin = 'ログインに成功しました！';
+const failureLogin = '問題が発生しました。もう一度、ログインしてください。';
+
+// メモ関連
+const eventTx = '種目';
+const weightTx = '重量';
+const repTx = '回数';
+const setTx = 'セット数';
+
+const eventFieldEmpty = '種目が入力されていません';
+const weightFieldEmpty = '重量が入力されていません';
+const repFieldEmpty = '回数が入力されていません';
+const setFieldEmpty = 'セット数が入力されていません';
