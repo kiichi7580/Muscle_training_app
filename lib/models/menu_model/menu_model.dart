434a7979227a8cafe915665a8fc94f23deb44e52
kiichi7580 <kiichi.nakazato0521@gmail.com>
2024-05-03 18:29:02 +0900

    MenuModelを使用して、memoListを取得できるようにした

--- lib/models/menu_model/menu_model.dart
+++ lib/models/menu_model/menu_model.dart
@@ -2,9 +2,18 @@ import 'dart:async';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:muscle_training_app/domain/memo.dart';
 
 class MenuModel extends ChangeNotifier {
   List<dynamic>? menus;
+  List<Memo> memoList = [];
+
+  List<Memo> get getMemoList => memoList;
+
+  void setMemoList(dynamic memo) {
+    this.memoList.add(memo);
+    notifyListeners();
+  }
 
   final Stream<QuerySnapshot> _usersStream = FirebaseFirestore.instance
       .collection('users')
