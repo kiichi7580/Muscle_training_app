288e58f3838a50e05c92ec18fe1b11e8954ee46f
kiichi7580 <kiichi.nakazato0521@gmail.com>
2024-05-07 19:58:22 +0900

    MenuWidgetでメニューを表示させるようにした

--- lib/view/calendar/bottom_sheet.dart
+++ lib/view/calendar/bottom_sheet.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:modal_bottom_sheet/modal_bottom_sheet.dart';
 import 'package:muscle_training_app/constant/colors.dart';
+import 'package:muscle_training_app/view/calendar/menu_widget.dart';
 
 void BottomSheetWidget(BuildContext context, {required String date}) async {
   final result = await showBarModalBottomSheet<dynamic>(
@@ -61,9 +62,10 @@ class _buildBottomSheetContentState extends State<_buildBottomSheetContent> {
                   minHeight: 0, // 任意の最小の高さ
                   maxHeight: 670, // 任意の最大の高さ
                 ),
-                // child: MenuWidget(
-                //   date: widget.date,
-                // ),
+                child: MenuWidget(
+                  context,
+                  widget.date,
+                ),
               ),
             ],
           ),
