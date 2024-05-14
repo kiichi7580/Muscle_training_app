71324c891233ecb0004441700444523bde3bd7e6
kiichi7580 <kiichi.nakazato0521@gmail.com>
2024-05-14 14:28:57 +0900

    フォロ中のユーザー一覧を表示するページを作成、リストの項目をタップするとそのユーザーのプロフィール画面に遷移するようにした

---
+++ lib/view/profile/user_relations/user_following_list_page.dart
@@ -0,0 +1,98 @@
+import 'package:flutter/material.dart';
+import 'package:muscle_training_app/constant/colors.dart';
+import 'package:muscle_training_app/models/profile_model/user_relations/user_following_model.dart';
+import 'package:muscle_training_app/view/profile/profile_page.dart';
+import 'package:provider/provider.dart';
+
+class UserFollowingListPage {
+  const UserFollowingListPage({
+    required this.userData,
+  });
+  final Map<dynamic, dynamic> userData;
+  Widget UserFollowingList(Map<dynamic, dynamic> userData) {
+    return ChangeNotifierProvider<UserFollowingModel>(
+      create: (_) => UserFollowingModel(userData)..getFollowingInfo(),
+      child: Scaffold(
+        body: Center(
+          child: Consumer<UserFollowingModel>(
+            builder: (context, model, child) {
+              final List<Map<dynamic, dynamic>>? userFollowing =
+                  model.userFollowing;
+
+              if (userFollowing == null) {
+                return Center(
+                  child: CircularProgressIndicator(
+                    color: linkBlue,
+                  ),
+                );
+              }
+
+              if (userFollowing.isEmpty) {
+                return Center(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Icon(
+                        Icons.person_add,
+                        color: blackColor,
+                        size: 45,
+                      ),
+                      SizedBox(
+                        height: 10,
+                      ),
+                      Text(
+                        '他のユーザーをフォローしましょう',
+                        style: TextStyle(
+                          color: blackColor,
+                        ),
+                      ),
+                    ],
+                  ),
+                );
+              }
+
+              return ListView.builder(
+                padding: EdgeInsets.symmetric(
+                  vertical: 16,
+                  horizontal: 8,
+                ),
+                itemCount: userFollowing.length,
+                itemBuilder: (context, index) {
+                  // final User user = Provider.of<UserProvider>(context).getUser;
+                  return InkWell(
+                    onTap: () => Navigator.of(context).push(
+                      MaterialPageRoute(
+                        builder: (context) => ProfilePage(
+                          uid: userFollowing[index]['uid'],
+                        ),
+                      ),
+                    ),
+                    child: ListTile(
+                      leading: userFollowing[index]['photoUrl'] ==
+                              'assets/icons/1024 1.png'
+                          ? CircleAvatar(
+                              backgroundImage: AssetImage(
+                                userFollowing[index]['photoUrl'],
+                              ),
+                              radius: 16,
+                            )
+                          : CircleAvatar(
+                              backgroundImage: NetworkImage(
+                                userFollowing[index]['photoUrl'],
+                              ),
+                              radius: 16,
+                            ),
+                      title: Text(
+                        userFollowing[index]['username'],
+                      ),
+                    ),
+                  );
+                },
+              );
+            },
+          ),
+        ),
+      ),
+    );
+  }
+}
