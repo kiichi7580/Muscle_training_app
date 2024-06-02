37c76a2501a6d93dac0c6df6eaca9a0b875cfc1a
kiichi7580 <kiichi.nakazato0521@gmail.com>
2024-06-02 23:34:32 +0900

    ファイルの配置場所をcalendarフォルダのwidgetsフォルダに移動、TableMemoWidgetで選択された日付のメニューを表示するようにした

---
+++ lib/view/calendar/widgets/menu_widget.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import 'package:muscle_training_app/domain/memo.dart';
+import 'package:muscle_training_app/models/memo_model/detail_memo_model.dart';
+import 'package:muscle_training_app/view/memo/widgets/table_memo_widget.dart';
+import 'package:provider/provider.dart';
+
+Widget MenuWidget(
+  BuildContext context,
+  String date,
+) {
+  int dateId = DetailMemoModel().dateToDateId(date);
+  return ChangeNotifierProvider<DetailMemoModel>(
+    create: (_) => DetailMemoModel()..fetchTableMemo(dateId),
+    child: Scaffold(
+      body: Center(
+        child: Consumer<DetailMemoModel>(
+          builder: (context, model, child) {
+            final List<Memo>? memos = model.memos;
+
+            if (memos == null) {
+              return const CircularProgressIndicator();
+            }
+
+            if (memos.isEmpty) {
+              return const Center(
+                child: Padding(
+                  padding: EdgeInsets.only(
+                    bottom: 150,
+                  ),
+                  child: Text(
+                    'メニューがありません',
+                    style: TextStyle(
+                      fontSize: 16,
+                    ),
+                  ),
+                ),
+              );
+            }
+            return TableMemoWidget(memos: memos);
+          },
+        ),
+      ),
+    ),
+  );
+}
