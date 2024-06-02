75dbb110e266639c3ea0126cef7fabfcc5f1e008
kiichi7580 <kiichi.nakazato0521@gmail.com>
2024-06-02 23:40:53 +0900

    メモ詳細ページの編集モードで、編集操作か削除操作かを選択させるダイアログを作成

---
+++ lib/view/memo/widgets/edit_or_delete_confirm_dialog.dart
@@ -0,0 +1,69 @@
+import 'package:flutter/material.dart';
+import 'package:muscle_training_app/constant/colors.dart';
+import 'package:muscle_training_app/constant/text_resorce.dart';
+
+class EditOrDeleteConfirmDialog extends StatelessWidget {
+  const EditOrDeleteConfirmDialog({
+    Key? key,
+    required this.memo,
+  });
+  final dynamic memo;
+
+  @override
+  Widget build(BuildContext context) {
+    return SimpleDialog(
+      backgroundColor: mainColor,
+      title: Row(
+        children: [
+          Text(
+            '「${memo['event']}」',
+            style: TextStyle(
+              fontSize: 18,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          Text(
+            'のメニューを',
+            style: TextStyle(
+              fontSize: 16,
+            ),
+          ),
+        ],
+      ),
+      children: [
+        SizedBox(
+          height: 24,
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            SimpleDialogOption(
+              child: Text(
+                editTx,
+                style: TextStyle(
+                  color: blackColor,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              onPressed: () {
+                Navigator.pop(context, editTx);
+              },
+            ),
+            SimpleDialogOption(
+              child: Text(
+                deleteTx,
+                style: TextStyle(
+                  color: deleteColor,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              onPressed: () {
+                Navigator.pop(context, deleteTx);
+              },
+            )
+          ],
+        )
+      ],
+    );
+  }
+}
