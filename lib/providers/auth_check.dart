3096c1bf20f4ab5135c4920e4dd47b43be4d3b07
kiichi7580 <kiichi.nakazato0521@gmail.com>
2024-06-05 22:15:51 +0900

    auth_check_page.dartからauth_check.dartにファイル名を変更

--- lib/providers/auth_check.dart
+++ lib/providers/auth_check.dart
@@ -2,13 +2,14 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:muscle_training_app/constant/colors.dart';
 import 'package:muscle_training_app/providers/auth_state_notifier.dart';
+import 'package:muscle_training_app/providers/save_login_date.dart';
 import 'package:muscle_training_app/resposive/mobile_screen_layout.dart';
 import 'package:muscle_training_app/resposive/resposive_layout.dart';
 import 'package:muscle_training_app/resposive/web_screen_layout.dart';
 import 'package:muscle_training_app/view/login/login_page.dart';
 
-class AuthCheckPage extends StatelessWidget {
-  const AuthCheckPage({Key? key});
+class AuthCheck extends StatelessWidget {
+  const AuthCheck({Key? key});
 
   @override
   Widget build(BuildContext context) {
@@ -20,6 +21,7 @@ class AuthCheckPage extends StatelessWidget {
             return userAsyncValue.when(
               data: (user) {
                 if (user != null) {
+                  saveLoginDate();
                   return ResponsiveLayout(
                     webScreenLayout: WebScreenLayout(),
                     mobileScreenLayout: MobileScreenLayout(uid: user.uid),
