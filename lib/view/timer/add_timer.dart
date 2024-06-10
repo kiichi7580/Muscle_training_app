2aaec31793a2825965c7055459fb2335ac7dfc8a
kiichi7580 <kiichi.nakazato0521@gmail.com>
2024-06-10 09:35:23 +0900

    CustomPickerを使用し、分と秒だけの時間ピッカーを使用、それに伴うデザインの修正

--- lib/view/timer/add_timer.dart
+++ lib/view/timer/add_timer.dart
@@ -1,6 +1,9 @@
 // Flutter imports:
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_datetime_picker_plus/flutter_datetime_picker_plus.dart';
+import 'package:intl/intl.dart';
+import 'package:muscle_training_app/view/timer/widgets/custom_picker.dart';
 
 // Package imports:
 import 'package:provider/provider.dart';
@@ -25,6 +28,7 @@ class _AddTimerPageState extends State<AddTimerPage> {
   final TextEditingController _minuteController = TextEditingController();
   final TextEditingController _secondController = TextEditingController();
   bool _isLoading = false;
+  DateTime selectedTime = DateTime(0, 0, 0, 0, 0, 0, 0, 0);
 
   Future<void> addTimer(
     BuildContext context,
@@ -81,18 +85,6 @@ class _AddTimerPageState extends State<AddTimerPage> {
             fontWeight: FontWeight.bold,
           ),
         ),
-        // actions: [
-        //   TextButton(
-        //     onPressed: () async {},
-        //     child: Text(
-        //       saveTx,
-        //       style: TextStyle(
-        //         color: blackColor,
-        //         fontWeight: FontWeight.bold,
-        //       ),
-        //     ),
-        //   ),
-        // ],
         foregroundColor: blackColor,
         backgroundColor: blueColor,
       ),
@@ -107,105 +99,98 @@ class _AddTimerPageState extends State<AddTimerPage> {
       child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          Padding(
-            padding: const EdgeInsets.all(8),
-            child: Container(
-              padding: const EdgeInsets.all(16),
-              child: Column(
-                children: [
-                  SizedBox(
-                    height: 90,
-                    width: 294,
-                    child: Padding(
-                      padding: const EdgeInsets.all(8),
-                      child: TextField(
-                        controller: _nameController,
-                        keyboardType: TextInputType.text,
-                        decoration: const InputDecoration(
-                          labelText: '名前',
-                          border: OutlineInputBorder(),
-                        ),
-                      ),
-                    ),
-                  ),
-                  const SizedBox(
-                    height: 8,
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      SizedBox(
-                        height: 90,
-                        width: 130,
-                        child: Padding(
-                          padding: const EdgeInsets.all(8),
-                          child: TextField(
-                            controller: _minuteController,
-                            keyboardType: TextInputType.number,
-                            decoration: const InputDecoration(
-                              hintText: '分数を入力',
-                              border: OutlineInputBorder(),
-                            ),
-                          ),
-                        ),
-                      ),
-                      const SizedBox(
-                        height: 90,
-                        child: Padding(
-                          padding: EdgeInsets.all(10),
-                          child: Text(
-                            ':',
-                            style: TextStyle(fontSize: 40),
-                          ),
-                        ),
-                      ),
-                      SizedBox(
-                        height: 90,
-                        width: 130,
-                        child: Padding(
-                          padding: const EdgeInsets.all(8),
-                          child: TextField(
-                            controller: _secondController,
-                            keyboardType: TextInputType.number,
-                            decoration: const InputDecoration(
-                              hintText: '秒数を入力',
-                              border: OutlineInputBorder(),
-                            ),
-                          ),
-                        ),
-                      ),
-                    ],
-                  ),
-                  const SizedBox(
-                    height: 50,
-                    width: double.infinity,
-                  ),
-                  SizedBox(
-                    height: 50,
-                    width: 200,
-                    child: CupertinoButton(
-                      color: blueColor,
-                      onPressed: () async {
-                        await addTimer(
-                          context,
-                          user.uid,
-                          _nameController.text,
-                          _minuteController.text,
-                          _secondController.text,
-                        );
-                      },
-                      child: const Text(
-                        '保存する',
-                        style: TextStyle(
-                          fontSize: 18,
-                          fontWeight: FontWeight.w600,
-                        ),
-                      ),
-                    ),
-                  ),
-                ],
+          Flexible(
+            flex: 1,
+            child: Container(),
+          ),
+          Text(
+            selectedTime == DateTime(0, 0, 0, 0, 0, 0, 0, 0)
+                ? '00:00'
+                : '${_minuteController.text}:${_secondController.text}',
+            style: TextStyle(
+              fontSize: 40,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          SizedBox(
+            height: 16,
+          ),
+          ElevatedButton(
+            style: ElevatedButton.styleFrom(
+              foregroundColor: heavyBlueColor,
+              backgroundColor: mainColor,
+              side: BorderSide(color: heavyBlueColor),
+            ),
+            onPressed: () {
+              DatePicker.showPicker(
+                context,
+                showTitleActions: true,
+                onChanged: (date) {
+                  selectedTime = date;
+                  setState(() {
+                    _minuteController.text = DateFormat("mm").format(date);
+                    _secondController.text = DateFormat("ss").format(date);
+                  });
+                },
+                onConfirm: (time) {
+                  setState(() {});
+                  _minuteController.text = DateFormat("mm").format(time);
+                  _secondController.text = DateFormat("ss").format(time);
+                },
+                pickerModel: CustomPicker(
+                  currentTime: selectedTime,
+                ),
+                locale: LocaleType.jp,
+              );
+            },
+            child: const Text(
+              '時間設定',
+            ),
+          ),
+          SizedBox(
+            height: MediaQuery.of(context).size.height * 0.1,
+          ),
+          SizedBox(
+            width: MediaQuery.of(context).size.width * 0.7,
+            child: Padding(
+              padding: const EdgeInsets.all(8),
+              child: TextField(
+                controller: _nameController,
+                keyboardType: TextInputType.text,
+                decoration: const InputDecoration(
+                  labelText: '名前',
+                ),
               ),
             ),
+          ),
+          SizedBox(
+            height: MediaQuery.of(context).size.height * 0.1,
+          ),
+          SizedBox(
+            height: 50,
+            width: 200,
+            child: CupertinoButton(
+              color: blueColor,
+              onPressed: () async {
+                await addTimer(
+                  context,
+                  user.uid,
+                  _nameController.text,
+                  _minuteController.text,
+                  _secondController.text,
+                );
+              },
+              child: const Text(
+                '保存する',
+                style: TextStyle(
+                  fontSize: 18,
+                  fontWeight: FontWeight.w600,
+                ),
+              ),
+            ),
+          ),
+          SizedBox(
+            height: MediaQuery.of(context).size.height * 0.2,
           ),
         ],
       ),
--- lib/view/timer/edit_timer.dart
+++ lib/view/timer/edit_timer.dart
@@ -1,6 +1,9 @@
 // Flutter imports:
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_datetime_picker_plus/flutter_datetime_picker_plus.dart';
+import 'package:intl/intl.dart';
+import 'package:muscle_training_app/view/timer/widgets/custom_picker.dart';
 
 // Package imports:
 import 'package:provider/provider.dart';
@@ -20,6 +23,8 @@ class EditTimerPage extends StatefulWidget {
 }
 
 class _EditTimerPageState extends State<EditTimerPage> {
+  // DateTime selectedTime = DateTime(0, 0, 0, 0, 0, 0, 0, 0);
+
   Future<void> upDate(BuildContext context, EditTimerModel model) async {
     try {
       model.startLoading();
@@ -43,150 +48,239 @@ class _EditTimerPageState extends State<EditTimerPage> {
     return ChangeNotifierProvider<EditTimerModel>(
       create: (_) => EditTimerModel(widget.timer),
       child: Scaffold(
-        backgroundColor: mainColor,
-        appBar: AppBar(
-          title: Text(
-            'タイマーを編集',
+          backgroundColor: mainColor,
+          appBar: AppBar(
+            title: Text(
+              'タイマーを編集',
+              style: TextStyle(
+                fontSize: 18,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            foregroundColor: blackColor,
+            backgroundColor: blueColor,
+          ),
+          body: buildBody(context)
+
+          // Center(
+          //   child: Consumer<EditTimerModel>(
+          //     builder: (context, model, child) {
+          //       return Center(
+          //         child: Column(
+          //           mainAxisAlignment: MainAxisAlignment.center,
+          //           children: [
+          //             Padding(
+          //               padding: const EdgeInsets.all(8),
+          //               child: Column(
+          //                 children: [
+          //                   SizedBox(
+          //                     height: 90,
+          //                     width: 294,
+          //                     child: Padding(
+          //                       padding: const EdgeInsets.all(8),
+          //                       child: TextField(
+          //                         keyboardType: TextInputType.text,
+          //                         controller: model.nameController,
+          //                         decoration: const InputDecoration(
+          //                           labelText: '名前',
+          //                           border: OutlineInputBorder(),
+          //                         ),
+          //                         onChanged: (text) {
+          //                           model
+          //                             ..timerName = text
+          //                             ..setTimerName(text);
+          //                         },
+          //                       ),
+          //                     ),
+          //                   ),
+          //                   const SizedBox(
+          //                     height: 30,
+          //                   ),
+          //                   Row(
+          //                     mainAxisAlignment: MainAxisAlignment.center,
+          //                     children: [
+          //                       SizedBox(
+          //                         height: 90,
+          //                         width: 130,
+          //                         child: Padding(
+          //                           padding: const EdgeInsets.all(8),
+          //                           child: TextField(
+          //                             keyboardType: TextInputType.number,
+          //                             controller: model.minuteController,
+          //                             decoration: const InputDecoration(
+          //                               labelText: '分数を入力',
+          //                               border: OutlineInputBorder(),
+          //                             ),
+          //                             onChanged: (text) {
+          //                               model
+          //                                 ..minute = text
+          //                                 ..setMinute(text);
+          //                             },
+          //                           ),
+          //                         ),
+          //                       ),
+          //                       const SizedBox(
+          //                         height: 90,
+          //                         child: Padding(
+          //                           padding: EdgeInsets.all(10),
+          //                           child: Text(
+          //                             ':',
+          //                             style: TextStyle(fontSize: 40),
+          //                           ),
+          //                         ),
+          //                       ),
+          //                       SizedBox(
+          //                         height: 90,
+          //                         width: 130,
+          //                         child: Padding(
+          //                           padding: const EdgeInsets.all(8),
+          //                           child: TextField(
+          //                             keyboardType: TextInputType.number,
+          //                             controller: model.secondController,
+          //                             decoration: const InputDecoration(
+          //                               labelText: '秒数を入力',
+          //                               border: OutlineInputBorder(),
+          //                             ),
+          //                             onChanged: (text) {
+          //                               model
+          //                                 ..second = text
+          //                                 ..setSecond(text);
+          //                             },
+          //                           ),
+          //                         ),
+          //                       ),
+          //                     ],
+          //                   ),
+          //                   const SizedBox(
+          //                     height: 50,
+          //                     width: double.infinity,
+          //                   ),
+          //                   SizedBox(
+          //                     height: 50,
+          //                     width: 200,
+          //                     child: CupertinoButton(
+          //                       color: blueColor,
+          //                       onPressed: () async {
+          //                         await upDate(context, model);
+          //                       },
+          //                       child: const Text(
+          //                         '変更する',
+          //                         style: TextStyle(
+          //                           fontSize: 18,
+          //                           fontWeight: FontWeight.w600,
+          //                         ),
+          //                       ),
+          //                     ),
+          //                   ),
+          //                 ],
+          //               ),
+          //             ),
+          //           ],
+          //         ),
+          //       );
+          //     },
+          //   ),
+          // ),
+          ),
+    );
+  }
+
+  Widget buildBody(BuildContext context) {
+    return Center(
+        child: Consumer<EditTimerModel>(builder: (context, model, child) {
+      int preMinute = int.parse(model.minuteController.text);
+      int preSecond = int.parse(model.secondController.text);
+      DateTime selectedTime = DateTime(0, 0, 0, 0, 0, 0, preMinute, preSecond);
+      return Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Flexible(
+            flex: 1,
+            child: Container(),
+          ),
+          Text(
+            '${model.minuteController.text}:${model.secondController.text}',
             style: TextStyle(
-              fontSize: 18,
+              fontSize: 40,
               fontWeight: FontWeight.bold,
             ),
           ),
-          // actions: [
-          //   TextButton(
-          //     onPressed: () async {},
-          //     child: Text(
-          //       upDateTx,
-          //       style: TextStyle(
-          //         color: blackColor,
-          //         fontWeight: FontWeight.bold,
-          //       ),
-          //     ),
-          //   ),
-          // ],
-          foregroundColor: blackColor,
-          backgroundColor: blueColor,
-        ),
-        body: Center(
-          child: Consumer<EditTimerModel>(
-            builder: (context, model, child) {
-              return Center(
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Padding(
-                      padding: const EdgeInsets.all(8),
-                      child: Column(
-                        children: [
-                          SizedBox(
-                            height: 90,
-                            width: 294,
-                            child: Padding(
-                              padding: const EdgeInsets.all(8),
-                              child: TextField(
-                                keyboardType: TextInputType.text,
-                                controller: model.nameController,
-                                decoration: const InputDecoration(
-                                  labelText: '名前',
-                                  border: OutlineInputBorder(),
-                                ),
-                                onChanged: (text) {
-                                  model
-                                    ..timerName = text
-                                    ..setTimerName(text);
-                                },
-                              ),
-                            ),
-                          ),
-                          const SizedBox(
-                            height: 30,
-                          ),
-                          Row(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: [
-                              SizedBox(
-                                height: 90,
-                                width: 130,
-                                child: Padding(
-                                  padding: const EdgeInsets.all(8),
-                                  child: TextField(
-                                    keyboardType: TextInputType.number,
-                                    controller: model.minuteController,
-                                    decoration: const InputDecoration(
-                                      labelText: '分数を入力',
-                                      border: OutlineInputBorder(),
-                                    ),
-                                    onChanged: (text) {
-                                      model
-                                        ..minute = text
-                                        ..setMinute(text);
-                                    },
-                                  ),
-                                ),
-                              ),
-                              const SizedBox(
-                                height: 90,
-                                child: Padding(
-                                  padding: EdgeInsets.all(10),
-                                  child: Text(
-                                    ':',
-                                    style: TextStyle(fontSize: 40),
-                                  ),
-                                ),
-                              ),
-                              SizedBox(
-                                height: 90,
-                                width: 130,
-                                child: Padding(
-                                  padding: const EdgeInsets.all(8),
-                                  child: TextField(
-                                    keyboardType: TextInputType.number,
-                                    controller: model.secondController,
-                                    decoration: const InputDecoration(
-                                      labelText: '秒数を入力',
-                                      border: OutlineInputBorder(),
-                                    ),
-                                    onChanged: (text) {
-                                      model
-                                        ..second = text
-                                        ..setSecond(text);
-                                    },
-                                  ),
-                                ),
-                              ),
-                            ],
-                          ),
-                          const SizedBox(
-                            height: 50,
-                            width: double.infinity,
-                          ),
-                          SizedBox(
-                            height: 50,
-                            width: 200,
-                            child: CupertinoButton(
-                              color: blueColor,
-                              onPressed: () async {
-                                await upDate(context, model);
-                              },
-                              child: const Text(
-                                '変更する',
-                                style: TextStyle(
-                                  fontSize: 18,
-                                  fontWeight: FontWeight.w600,
-                                ),
-                              ),
-                            ),
-                          ),
-                        ],
-                      ),
-                    ),
-                  ],
+          SizedBox(
+            height: 16,
+          ),
+          ElevatedButton(
+            style: ElevatedButton.styleFrom(
+              foregroundColor: heavyBlueColor,
+              backgroundColor: mainColor,
+              side: BorderSide(color: heavyBlueColor),
+            ),
+            onPressed: () {
+              DatePicker.showPicker(
+                context,
+                showTitleActions: true,
+                onChanged: (date) {
+                  selectedTime = date;
+                  setState(() {
+                    model.minuteController.text = DateFormat("mm").format(date);
+                    model.secondController.text = DateFormat("ss").format(date);
+                  });
+                },
+                onConfirm: (time) {
+                  setState(() {});
+                  model.minuteController.text = DateFormat("mm").format(time);
+                  model.secondController.text = DateFormat("ss").format(time);
+                },
+                pickerModel: CustomPicker(
+                  currentTime: selectedTime,
                 ),
+                locale: LocaleType.jp,
               );
             },
+            child: const Text(
+              '時間設定',
+            ),
           ),
-        ),
-      ),
-    );
+          SizedBox(
+            height: MediaQuery.of(context).size.height * 0.1,
+          ),
+          SizedBox(
+            width: MediaQuery.of(context).size.width * 0.7,
+            child: Padding(
+              padding: const EdgeInsets.all(8),
+              child: TextField(
+                controller: model.nameController,
+                keyboardType: TextInputType.text,
+                decoration: const InputDecoration(
+                  labelText: '名前',
+                ),
+              ),
+            ),
+          ),
+          SizedBox(
+            height: MediaQuery.of(context).size.height * 0.1,
+          ),
+          SizedBox(
+            height: 50,
+            width: 200,
+            child: CupertinoButton(
+              color: blueColor,
+              onPressed: () async {
+                await upDate(context, model);
+              },
+              child: const Text(
+                '保存する',
+                style: TextStyle(
+                  fontSize: 18,
+                  fontWeight: FontWeight.w600,
+                ),
+              ),
+            ),
+          ),
+          SizedBox(
+            height: MediaQuery.of(context).size.height * 0.2,
+          ),
+        ],
+      );
+    }));
   }
 }
